2a7f8d14a078afce414c6dbd7c63b32267f1e348
Chris Alberti <christopherjalberti@gmail.com>
2018-04-20 17:42:36 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,4 +7,9 @@ def key_for_min_value(name_hash)
 
   name_hash.each do |key, value|
     if smallest_value == nil || value < smallest_value
+      smallest_key = key
+      smallest_value = value
+    end
+  end
+  smallest_key
 end
